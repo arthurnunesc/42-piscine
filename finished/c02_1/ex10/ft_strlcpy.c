a4fce1d97d0dfa0b83c0b7ed554cd0ee58634181
Arthur Nunes Correa <anunes-c@cbr6s3.42barcelona.com>
2023-02-23 18:46:00 +0100

    Add second try of c02

---
+++ finished/c02_1/ex10/ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anunes-c <anunes-c@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 17:48:20 by anunes-c          #+#    #+#             */
+/*   Updated: 2023/02/22 19:12:22 by anunes-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (i < size - 1 && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
---
+++ finished/c02_1/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anunes-c <anunes-c@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 13:34:53 by anunes-c          #+#    #+#             */
+/*   Updated: 2023/02/23 18:39:03 by anunes-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_int_to_hex(int number)
+{
+	int	remainder;
+	int	division;
+
+	division = (number / 16) + 48;
+	remainder = number % 16;
+	write(1, "\\", 1);
+	write(1, &division, 1);
+	if (remainder > 9)
+	{
+		remainder += 87;
+		write(1, &remainder, 1);
+	}
+	else
+	{
+		remainder += 48;
+		write(1, &remainder, 1);
+	}
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] >= 0 && str[i] < 32) || str[i] == 127)
+			ft_int_to_hex(str[i]);
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+}
